5343c86e690bcbacb09e1d89f30a3c940182c595
Gremoryyx <hiimyino0503@gmail.com>
2024-06-16 02:26:06 +0800

    :sparkle: landing

---
+++ src/components/CTA.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<div class="flex items-center gap-4">
+    <button class={"special-btn"}>
+        <p class="text-base sm:text-lg md:text-xl">
+            Contact Us
+        </p>
+    </button>
+    <button class="special-btn-dark">
+        <p class="text-base sm-text-lg md:text-xl">
+            Get Started &rarr;
+        </p>
+    </button>
+</div>
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,10 +1,45 @@
 <script>
   import SectionWrapper from "./SectionWrapper.svelte";
   import Header from "./Header.svelte";
+  import Cta from "./CTA.svelte";
+  import * as Icon from "lucide-svelte";
 </script>
 
 
 <SectionWrapper>
     <Header />
-    quack
+    <div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
+        <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl mx-auto text-center w-full max-w-[1200px] font-semibold">
+            <span class="text-indigo-400">
+                Gym
+            </span>
+            Training
+            <span class="text-slate-600 line-through">Is Hard</span>
+            <br>
+            Just Got
+            <span class="text-indigo-400">
+                Easier
+            </span>
+        </h2>
+        <p class="text-xl sm:text-2xl md:text-3xl text-center mx-auto w-full max-w-[1000px]">
+            Less thinking & more doing. We're the trainer in your pocket full of 
+            <span class="italic">personalized workouts, exercise explainations, analytics</span>
+            and much much more.
+        </p>
+        <Cta />
+        <div class="flex items-center justify-center gap-2 text-base">
+            <p>4.6</p>
+            { #each [0,1,2,3,4] as index }
+                <div class="grid place-items-center-relative">
+                    <div class={
+                            "grid place-items-center " + 
+                            (index === 4 ? "w-3/5 overflow-hidden": " ")
+                        }>
+                            <Icon.StarIcon color="#E6F285" />
+                        </div>
+                </div>
+            {/each}
+            <p>500+</p>
+        </div>
+    </div>
 </SectionWrapper>
